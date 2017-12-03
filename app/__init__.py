e79fd393ad11c9160d0c627f64df6b3d3f9d2eac
Ross Timson <ross@rosstimson.com>
2017-12-03 17:29:53 +0000

    Add Flask Debug Toolbar.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
+from flask_debugtoolbar import DebugToolbarExtension
 from config import config
 
 
 bootstrap = Bootstrap()
+toolbar = DebugToolbarExtension()
 
 
 def create_app(config_name):
@@ -12,6 +14,7 @@ def create_app(config_name):
     config[config_name].init_app(app)
 
     bootstrap.init_app(app)
+    toolbar.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
